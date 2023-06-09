fdd08a11e5d44a2215caa22688f06ef67657e953
Lauro Figueiredo <lauroazevedof@outlook.com>
2023-06-09 16:01:16 -0300

    small hotfix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  def configure_permitted_params
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :password_confirmation, :name) }
+    devise_parameter_sanitizer.for(:edit) { |u| u.permit(:email, :password, :password_confirmation, :name) }
+  end
 end
