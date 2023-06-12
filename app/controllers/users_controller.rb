47a1d3f8494248d3cd45502867e2c0670c76b9fb
Lauro Figueiredo <lauroazevedof@outlook.com>
2023-06-12 15:48:13 -0300

    Users show and charts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,33 +3,40 @@ class UsersController < ApplicationController
   def show
     @user = current_user
     @group = @user.group
-    @incomes = Income.where(group: @group).order(:date)
-    @outcomes = Outcome.where(group: @group).order(:value)
-    @chart_data = {
-      labels: ['Entradas (R$)', 'Saídas (R$)'],
-      datasets: [{
-        label: '',
-        backgroundColor: ['#90eebfa9', '#ff7883a9'],
-        borderColor: ['green', 'red'],
-        type: 'pie',
-        data: [@incomes.sum(:value), @outcomes.sum(:value)]
-      }],
-    }
-
-    @chart_options = {
-      plugins: {
-        title: {
-          display: true,
-          text: 'Entradas e Sáidas (R$)'
-        }
-      },
-      scales: {
-        yAxes: [{
-          ticks: {
-            beginAtZero: true
-          }
-        }]
+    if @group
+      @incomes = Income.where(group: @group).order(:date)
+      @outcomes = Outcome.where(group: @group).order(:value)
+      @total_income_per_group = @user.group.memberships.map { |membership| membership.user.incomes.sum(:value) }.sum
+      @total_outcome_per_group = @user.group.memberships.map { |membership| membership.user.outcomes.sum(:value) }.sum
+      @chart_data = {
+        labels: ['Entradas (R$)', 'Saídas (R$)'],
+        datasets: [{
+          label: '',
+          backgroundColor: ['#90eebfa9', '#ff7883a9'],
+          borderColor: ['green', 'red'],
+          type: 'pie',
+          data: [@total_income_per_group, @total_outcome_per_group]
+        }],
       }
-    }
+
+      @chart_options = {
+        plugins: {
+          legend: {
+            display: false
+          },
+          title: {
+            display: true,
+            text: 'Entradas e Sáidas (R$)'
+          }
+        },
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            }
+          }]
+        }
+      }
+    end
   end
 end
