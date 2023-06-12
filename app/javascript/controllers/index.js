425a8ecc59b9533d0a7d1178b95a6b7a8a0abe90
Lauro Figueiredo <lauroazevedof@outlook.com>
2023-06-12 16:32:33 -0300

    Chart fix

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,6 +4,9 @@
 
 import { application } from "./application"
 
+import Chart from 'stimulus-chartjs'
+application.register('chart', Chart)
+
 import FlatpickrController from "./flatpickr_controller"
 application.register("flatpickr", FlatpickrController)
 
