1f72b8cb031a47f386886698edb252f189aca0cb
Lauro Figueiredo <lauroazevedof@outlook.com>
2023-06-15 15:11:46 -0300

    redirect user

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,14 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    unless current_user == nil
+      redirect_to user_path(current_user)
+    end
+  end
+
+  def go_to_user
+    unless current_user == nil
+      redirect_to user_path(current_user)
+    end
   end
 end
